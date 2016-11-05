77a90d2fdea53458a82460a29ae26875387fd1d2
Andreas Kloeckner <inform@tiker.net>
2016-11-05 11:08:49 -0500

    Use mingwpy to build on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,2 @@
 python configure.py --cl-inc-dir=%LIBRARY_INC% --cl-lib-dir=%LIBRARY_LIB%
-python setup.py install --single-version-externally-managed --record record.txt
+python setup.py install  --compiler=mingw32 --single-version-externally-managed --record record.txt

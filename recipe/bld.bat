1d5594933c6ea15039148ff8c76c46cf3d1f6af3
Andreas Kloeckner <inform@tiker.net>
2016-11-06 13:01:13 -0600

    Add missing runtime dep

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,4 +4,4 @@ if errorlevel 1 exit 1
 if errorlevel 1 exit 1
 "%PYTHON%" setup.py build_ext --compiler=mingw32
 if errorlevel 1 exit 1
-"%PYTHON%" setup.py install  --single-version-externally-managed --record record.txt
+"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt

151f17f7d690ab182b33105ee4c07a23cc8f1fb4
Matthias Diener <mdiener@illinois.edu>
2024-05-06 10:22:47 -0500

    change stdlib, rerender

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,6 @@ set -ex
 
 ./configure.py --cl-inc-dir=$PREFIX/include --cl-lib-dir=$PREFIX/lib --cl-libname=OpenCL
 
+cat siteconf.py
+
 python setup.py install --single-version-externally-managed --record record.txt

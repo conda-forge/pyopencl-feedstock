1cb14c6beb842a1b3cbda60c68d6b991281936a7
Matthias Diener <mdiener@illinois.edu>
2024-06-06 17:22:15 -0500

    fix(?) Win build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,5 @@
-if "%CONDA_PY%"=="27" (
-  set COMPILER_FLAG=--compiler=mingw32
-) else if "%CONDA_PY%"=="34" (
-  set COMPILER_FLAG=--compiler=mingw32
-) else (
-  set COMPILER_FLAG=
-)
-
 set CL_INC_DIR=%LIBRARY_INC%
 set CL_LIB_DIR=%LIBRARY_LIB%
 
+"%PYTHON%" -m pip install -vv .
 if errorlevel 1 exit 1
-"%PYTHON%" setup.py build %COMPILER_FLAG%
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py build_ext %COMPILER_FLAG%
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
